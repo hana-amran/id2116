a9ae9e2aee29e1e1d680535cd8fd6f342a06edc0
Hana Amran <hanaamran@Hanas-MacBook-Pro.local>
2020-02-02 09:12:09 +0800

    Group site

---
+++ Group Folder/p5js/PhotoFrame_2020_01_31_09_34_22/sketch.js
@@ -0,0 +1,49 @@
+let image_filelist = ['images/orange.jpg','images/apple.jpg','images/banana.jpg'];
+    let imagelist = [];
+
+    function preload() {
+
+      for (let filename of image_filelist) {
+
+        imagelist.push(loadImage(filename));
+      }
+
+      print(imagelist.length + " images are loaded into the lists!");
+
+    }
+
+    function draw() {
+      show();
+    }
+
+    let current_image = 0;
+
+    function show() {
+      image(imagelist[current_image], 0, 0);
+    }
+
+    function next() {
+      current_image = current_image + 1;
+
+      if (current_image > imagelist.length - 1) {
+        current_image = 0
+      }
+      print("next image is" + current_image);
+    }
+
+    function prev() {
+      current_image = imagelist.length - 1;
+    }
+
+    let nextButton;
+    let prevButton;
+
+    function setup() {
+      createCanvas(600, 400);
+
+      prevButton = createButton('prev');
+      prevButton.mousePressed(prev);
+
+      nextButton = createButton('next');
+      nextButton.mousePressed(next);
+    }
