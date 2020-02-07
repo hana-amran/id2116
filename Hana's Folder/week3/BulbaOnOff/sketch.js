88659a760a641e0916bf5f1c259ffafba366ae65
Hana Amran <your@emailaddress.com>
2020-02-07 15:43:08 +0800

    Group site

---
+++ Hana's Folder/week3/BulbaOnOff/sketch.js
@@ -0,0 +1,40 @@
+let bulbOn;
+let bulbOff;
+
+let brightness = 1;
+  
+function setup() {
+  createCanvas(400, 400);
+  
+  bulbOn = loadImage("images/bulb_on.png");
+  bulbOff = loadImage("images/bulb_off.png");
+}
+
+function draw() {
+  background(200);
+  
+  imageMode(CENTER);
+  if(brightness == 1){
+    image(bulbOn,width/2,height/2,300,300);
+    image(bulbOff,width/2,height/2,300,300);
+  }
+}
+
+function on(){
+  brightness = 1;
+  print("the bulb is on!");
+}
+
+function off(){
+  brightness = 0;
+  print("the bulb is off!");
+}
+        
+function mousePressed(){
+  on();
+}
+
+function mouseReleased(){
+  off();
+}
+
